f0af0b52d16cdb00935410dfd3fe61c27123c1d8
Ryosuke Ito <suzusuke0806@gmail.com>
2022-11-18 17:18:24 +0900

    Initial Commit

---
+++ swiftUITry/swiftUITryApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUITryApp.swift
+//  swiftUITry
+//
+//  Created by suzu on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct swiftUITryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
