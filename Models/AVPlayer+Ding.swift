070ad1137f6871dd6164313cf46ca7ebace0154d
suzusuke <suzusuke0806@gmail.com>
2022-11-23 10:18:21 +0900

    [chapter5]finished Managing state and life cycle

---
+++ Models/AVPlayer+Ding.swift
@@ -0,0 +1,13 @@
+//
+// Created by suzu on 2022/11/23.
+//
+
+import Foundation
+import AVFoundation
+
+extension AVPlayer {
+    static let sharedDingPlayer: AVPlayer = {
+        guard let url = Bundle.main.url(forResource: "ding", withExtension: "wav") else { fatalError("Failed to find sound file.") }
+        return AVPlayer(url: url)
+    }()
+}
